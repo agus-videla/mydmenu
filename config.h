2b2ab3f9224525becf96fcba93810d83cc505ccb
Agustín Octavio Videla <agustinvidela0198@gmail.com>
2022-04-19 13:27:58 -0300

    init

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+	"SourceCodePro:size=17",
+	"Noto Color Emoji:pixelsize=15:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
@@ -25,6 +26,9 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
